48ed8e07801c3c6ec5bdc158fa4e191f071be3c3
ilexwg <ilexwg@gmail.com>
2021-09-14 17:15:23 +0800

    change http to https

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://my-json-server.typicode.com/ilexwg/real-world-vue3-demo',
+  baseURL: 'https://my-json-server.typicode.com/ilexwg/real-world-vue3-demo',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
