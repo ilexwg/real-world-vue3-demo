ab3937fd59d30b6136efb4843363e0b3fd9da628
ilexwg <ilexwg@gmail.com>
2021-09-15 14:33:52 +0800

    add pagination

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -2,12 +2,33 @@
   <h1>Events for Good</h1>
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+
+    <div class="pagination">
+      <router-link
+        id="page-prev"
+        :to="{ name: 'EventList', query: { page: page - 1 } }"
+        rel="prev"
+        v-if="page != 1"
+      >
+        &#60; Previous
+      </router-link>
+
+      <router-link
+        id="page-next"
+        :to="{ name: 'EventList', query: { page: page + 1 } }"
+        rel="next"
+        v-if="hasNextPage"
+      >
+        Next &#62;
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue';
 import EventService from '@/services/EventService.js';
+import { watchEffect } from 'vue';
 
 export default {
   name: 'EventList',
@@ -16,20 +37,39 @@ export default {
     EventCard,
   },
 
+  props: {
+    page: {
+      type: Number,
+      default: 1,
+    },
+  },
+
   data() {
     return {
       events: null,
+      totalEvents: 0,
     };
   },
 
+  computed: {
+    hasNextPage() {
+      const totalPages = Math.ceil(this.totalEvents / 2);
+      return this.page < totalPages;
+    },
+  },
+
   created() {
-    EventService.getEvents()
-      .then((response) => {
-        this.events = response.data;
-      })
-      .catch((error) => {
-        console.log(error, '👀');
-      });
+    watchEffect(() => {
+      this.events = null;
+      EventService.getEvents(2, this.page)
+        .then((response) => {
+          this.events = response.data;
+          this.totalEvents = parseInt(response.headers['x-total-count']);
+        })
+        .catch((error) => {
+          console.log(error, '👀');
+        });
+    });
   },
 };
 </script>
@@ -39,5 +79,22 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+.pagination {
+  display: flex;
+  width: 290px;
+}
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+
+#page-prev {
+  text-align: left;
+}
+
+#page-next {
+  text-align: right;
 }
 </style>
