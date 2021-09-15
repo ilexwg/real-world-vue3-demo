d762265476a1f13c5c939a808ae96915708f0c49
ilexwg <ilexwg@gmail.com>
2021-09-15 14:34:09 +0800

    add nested routes to show event

---
+++ src/views/event/Edit.vue
@@ -0,0 +1,11 @@
+<template>
+  <p>Edit the event here</p>
+</template>
+
+<script>
+export default {
+  name: 'EventEdit',
+
+  props: ['event'],
+};
+</script>
---
+++ src/views/event/Register.vue
@@ -0,0 +1,11 @@
+<template>
+  <p>Register for the event here</p>
+</template>
+
+<script>
+export default {
+  name: 'EventRegister',
+
+  props: ['event'],
+};
+</script>
