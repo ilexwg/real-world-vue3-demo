e2a98f9df9fa4b49e372d37be8d7076ae8c76f86
ilexwg <ilexwg@gmail.com>
2021-09-15 15:36:33 +0800

    add a simple register demo

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,5 +1,6 @@
 <template>
   <p>Register for the event here</p>
+  <button @click="register">Register Me!</button>
 </template>
 
 <script>
@@ -7,5 +8,14 @@ export default {
   name: 'EventRegister',
 
   props: ['event'],
+
+  methods: {
+    register() {
+      this.$router.replace({
+        name: 'EventDetails',
+        params: { id: this.event.id },
+      });
+    },
+  },
 };
 </script>
